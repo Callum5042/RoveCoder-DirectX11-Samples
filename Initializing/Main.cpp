f41e2fb8b77fbe79c57d2866c989d813918920be
Callum Anning <CallumAnning@hotmail.co.uk>
2024-01-31 13:24:11 +0000

    Added check for memory leaks

--- Initializing/Main.cpp
+++ Initializing/Main.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char** argv)
 {
+#ifdef _DEBUG
+	// Detect memory leaks
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	// _CrtSetBreakAlloc(972);
+#endif
+
 	std::unique_ptr<Application> application = std::make_unique<Application>();
 	return application->Execute();
 }
