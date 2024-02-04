b1dd724b0210e3c3c4d19181c8bdceb34d7f66b8
Callum <CallumAnning@hotmail.co.uk>
2024-02-04 13:23:15 +0000

    Camera sample (#9)
    * Added camera project
    
    * Added camera
    
    * Added camera
    
    * Rotated camera
    
    * Added cube
    
    * Added wireframe

---
+++ Camera/Shader.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+// This include is requires for using DirectX smart pointers (ComPtr)
+#include <wrl\client.h>
+using Microsoft::WRL::ComPtr;
+
+class Renderer;
+
+class Shader
+{
+	Renderer* m_Renderer = nullptr;
+
+public:
+	Shader(Renderer* renderer);
+	virtual ~Shader() = default;
+
+	// Load the shader
+	void Load();
+
+	// Bind shader to the pipeline
+	void Use();
+
+	// Update the model view projection constant buffer
+	void UpdateModelViewProjectionBuffer(DirectX::XMMATRIX matrix);
+
+private:
+	// Create vertex shader
+	void LoadVertexShader();
+	ComPtr<ID3D11VertexShader> m_VertexShader = nullptr;
+	ComPtr<ID3D11InputLayout> m_VertexLayout = nullptr;
+
+	// Create pixel shader
+	void LoadPixelShader();
+	ComPtr<ID3D11PixelShader> m_PixelShader = nullptr;
+
+	// ModelViewProjection constant buffer
+	ComPtr<ID3D11Buffer> m_ModelViewProjectionConstantBuffer = nullptr;
+	void CreateWorldViewProjectionConstantBuffer();
+};
