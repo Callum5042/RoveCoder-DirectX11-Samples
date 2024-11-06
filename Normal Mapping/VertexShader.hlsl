4f269e0191b8eb5b1fa7b4225f7f57eb6a8f8f1c
Callum Anning <CallumAnning@hotmail.co.uk>
2024-11-06 21:41:56 +0000

    Added transforming tangents by the world matrix

--- Normal Mapping/VertexShader.hlsl
+++ Normal Mapping/VertexShader.hlsl
@@ -8,7 +8,7 @@ PixelInput main(VertexInput input)
 	// Transform to homogeneous clip space
     pixel_input.positionClipSpace = mul(float4(input.position, 1.0f), cModelViewProjection);
 
-    // Transform to world space.
+    // Transform to world space
     pixel_input.position = input.position;
     
     // Pass the texture's UV coordinates
@@ -16,8 +16,7 @@ PixelInput main(VertexInput input)
     
     // Transform the normals and tangents by the inverse world space
     pixel_input.normal = mul(input.normal, (float3x3) cModelInverse).xyz;
-    pixel_input.tangent = input.tangent;
-    // pixel_input.tangent = mul(input.tangent, (float3x3) cModel);
+    pixel_input.tangent = mul(input.tangent, (float3x3) cModel);
     
     return pixel_input;
 }
