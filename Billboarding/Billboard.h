6bb764765d5c82c783ed1f76b46d3c455886bb0c
Callum <CallumAnning@hotmail.co.uk>
2025-01-11 15:50:39 +0000

    Billboarding sample (#25)
    * Added billboarding project
    
    * Made model flatter
    
    * Added Billboard shader
    
    * Added billboard skeleton
    
    * Added texture support
    
    * Added basic blending to see the tree better
    
    * Changed textures

---
+++ Billboarding/Billboard.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <d3d11.h>
+#include <vector>
+
+// This include is requires for using DirectX smart pointers (ComPtr)
+#include <wrl\client.h>
+using Microsoft::WRL::ComPtr;
+
+class Renderer;
+
+struct BillboardVertex
+{
+	// Vertex position
+	float x = 0;
+	float y = 0;
+	float z = 0;
+
+	// Size
+	float width = 0;
+	float height = 0;
+};
+
+class Billboard
+{
+	Renderer* m_Renderer = nullptr;
+
+public:
+	Billboard(Renderer* renderer);
+	virtual ~Billboard() = default;
+
+	// Create device
+	void Create();
+
+	// Render the model
+	void Render();
+
+private:
+
+	// Vertex buffer
+	ComPtr<ID3D11Buffer> m_VertexBuffer = nullptr;
+	void CreateVertexBuffer();
+
+	// Vertices
+	std::vector<BillboardVertex> m_Vertices;
+
+	// Texture buffer
+	void LoadTexture();
+	ComPtr<ID3D11ShaderResourceView> m_DiffuseTexture = nullptr;
+};
---
+++ Billboarding/BillboardShader.cpp
@@ -0,0 +1,92 @@
+#include "BillboardShader.h"
+#include "Renderer.h"
+
+#include <Windows.h>
+#include "CompiledBillboardPixelShader.hlsl.h"
+#include "CompiledBillboardVertexShader.hlsl.h"
+#include "CompiledBillboardGeometryShader.hlsl.h"
+
+BillboardShader::BillboardShader(Renderer* renderer) : m_Renderer(renderer)
+{
+}
+
+void BillboardShader::Load()
+{
+	this->LoadVertexShader();
+	this->LoadPixelShader();
+	this->LoadGeometryShader();
+	this->CreateWorldConstantBuffer();
+}
+
+void BillboardShader::Use()
+{
+	ID3D11DeviceContext* context = m_Renderer->GetDeviceContext();
+
+	// Bind the input layout to the pipeline's Input Assembler stage
+	context->IASetInputLayout(m_VertexLayout.Get());
+
+	// Bind the vertex shader to the pipeline's Vertex Shader stage
+	context->VSSetShader(m_VertexShader.Get(), nullptr, 0);
+
+	// Bind the geometry shader to the pipeline's Geometry Shader stage
+	context->GSSetShader(m_GeometryShader.Get(), nullptr, 0);
+
+	// Bind the pixel shader to the pipeline's Pixel Shader stage
+	context->PSSetShader(m_PixelShader.Get(), nullptr, 0);
+
+	// Bind the world constant buffer to the vertex shader
+	const int constant_buffer_slot = 0;
+	context->VSSetConstantBuffers(constant_buffer_slot, 1, m_WorldConstantBuffer.GetAddressOf());
+
+	// Bind the world constant buffer to the geometry shader
+	context->GSSetConstantBuffers(0, 1, m_WorldConstantBuffer.GetAddressOf());
+}
+
+void BillboardShader::LoadVertexShader()
+{
+	ID3D11Device* device = m_Renderer->GetDevice();
+
+	// Create the vertex shader
+	DX::Check(device->CreateVertexShader(g_BillboardVertexShader, sizeof(g_BillboardVertexShader), nullptr, m_VertexShader.ReleaseAndGetAddressOf()));
+
+	// Describe the memory layout
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "SIZE", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+
+	UINT number_elements = ARRAYSIZE(layout);
+	DX::Check(device->CreateInputLayout(layout, number_elements, g_BillboardVertexShader, sizeof(g_BillboardVertexShader), m_VertexLayout.ReleaseAndGetAddressOf()));
+}
+
+void BillboardShader::LoadPixelShader()
+{
+	ID3D11Device* device = m_Renderer->GetDevice();
+	device->CreatePixelShader(g_BillboardPixelShader, sizeof(g_BillboardPixelShader), nullptr, m_PixelShader.ReleaseAndGetAddressOf());
+}
+
+void BillboardShader::LoadGeometryShader()
+{
+	ID3D11Device* device = m_Renderer->GetDevice();
+	DX::Check(device->CreateGeometryShader(g_BillboardGeometryShader, sizeof(g_BillboardGeometryShader), nullptr, m_GeometryShader.ReleaseAndGetAddressOf()));
+}
+
+void BillboardShader::CreateWorldConstantBuffer()
+{
+	ID3D11Device* device = m_Renderer->GetDevice();
+
+	// Create world constant buffer
+	D3D11_BUFFER_DESC bd = {};
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(WorldBuffer);
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+
+	DX::Check(device->CreateBuffer(&bd, nullptr, m_WorldConstantBuffer.ReleaseAndGetAddressOf()));
+}
+
+void BillboardShader::UpdateWorldConstantBuffer(const WorldBuffer& worldBuffer)
+{
+	ID3D11DeviceContext* context = m_Renderer->GetDeviceContext();
+	context->UpdateSubresource(m_WorldConstantBuffer.Get(), 0, nullptr, &worldBuffer, 0, 0);
+}
