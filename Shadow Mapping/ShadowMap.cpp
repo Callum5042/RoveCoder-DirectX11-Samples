956e0810cb621ab66a70b905942c236bce07f69a
Callum Anning <CallumAnning@hotmail.co.uk>
2026-01-10 12:41:53 +0000

    Fixed memory leak and updated to use 32-bit texture

--- Shadow Mapping/ShadowMap.cpp
+++ Shadow Mapping/ShadowMap.cpp
@@ -3,7 +3,6 @@
 ShadowMap::ShadowMap(Renderer* renderer) : m_Renderer(renderer)
 {
 	this->CreateShadowMapTexture();
-	this->CreateShadowMapTexture();
 	this->CreateRasterModeBackCull();
 	this->CreateShadowSampler();
 }
@@ -23,7 +22,7 @@ void ShadowMap::Bind()
 	D3D11_VIEWPORT viewport = {};
 	viewport.Width = static_cast<float>(m_ShadowMapTextureSize);
 	viewport.Height = static_cast<float>(m_ShadowMapTextureSize);
-	viewport.MinDepth = 0.0f;
+	viewport.MinDepth = 0.0f; 
 	viewport.MaxDepth = 1.0f;
 	viewport.TopLeftX = 0;
 	viewport.TopLeftY = 0;
@@ -40,13 +39,14 @@ void ShadowMap::CreateShadowMapTexture()
 	ID3D11Device* device = m_Renderer->GetDevice();
 
 	// Create shadow map texture
-	D3D11_TEXTURE2D_DESC texture_desc = {};
+	D3D11_TEXTURE2D_DESC texture_desc = {}; 
 	texture_desc.Width = static_cast<UINT>(m_ShadowMapTextureSize);
 	texture_desc.Height = static_cast<UINT>(m_ShadowMapTextureSize);
+	texture_desc.MipLevels = 1;
 	texture_desc.ArraySize = 1;
 	texture_desc.SampleDesc.Count = 1;
 	texture_desc.SampleDesc.Quality = 0;
-	texture_desc.Format = DXGI_FORMAT_R24G8_TYPELESS;
+	texture_desc.Format = DXGI_FORMAT_R32_TYPELESS;
 	texture_desc.Usage = D3D11_USAGE_DEFAULT;
 	texture_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
 
@@ -55,14 +55,14 @@ void ShadowMap::CreateShadowMapTexture()
 
 	// Create depth stencil view
 	D3D11_DEPTH_STENCIL_VIEW_DESC depth_view_desc = {};
-	depth_view_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depth_view_desc.Format = DXGI_FORMAT_D32_FLOAT;
 	depth_view_desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
 
 	DX::Check(device->CreateDepthStencilView(texture.Get(), &depth_view_desc, m_DepthStencilView.GetAddressOf()));
 
 	// Create shadow resource
 	D3D11_SHADER_RESOURCE_VIEW_DESC shader_resource_view_desc = {};
-	shader_resource_view_desc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+	shader_resource_view_desc.Format = DXGI_FORMAT_R32_FLOAT;
 	shader_resource_view_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	shader_resource_view_desc.Texture2D.MipLevels = 1;
 
@@ -73,7 +73,7 @@ void ShadowMap::CreateRasterModeBackCull()
 {
 	D3D11_RASTERIZER_DESC desc = {};
 	desc.AntialiasedLineEnable = false;
-	desc.CullMode = D3D11_CULL_FRONT;
+	desc.CullMode = D3D11_CULL_BACK;
 	desc.FillMode = D3D11_FILL_SOLID;
 	desc.DepthClipEnable = false;
 	desc.FrontCounterClockwise = false;
